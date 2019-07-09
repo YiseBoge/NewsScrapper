4a134e78e4854d73ce460536703281baa2026ca2
yiseboge <Ethiopia1!>
2019-07-09 14:41:19 +0300

    initial commit

---
+++ src/main/java/com/se/NewsScrapper/NewsScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.se.NewsScrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsScrapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsScrapperApplication.class, args);
+    }
+
+}
